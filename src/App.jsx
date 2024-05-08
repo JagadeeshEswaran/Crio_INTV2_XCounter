ed2c60b740b2be6859fb4336e8d6fac73ff9e32e
jaga172023 <jaga.101602@gmail.com>
2024-05-08 23:31:44 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+function App() {
+	const [count, setCount] = useState(0);
+
+	return (
+		<>
+			<h1>Counter App</h1>
+
+			<p>{count}</p>
+
+			<article>
+				<button onClick={() => setCount((prevCount) => prevCount + 1)}>
+					Increment
+				</button>
+				<button onClick={() => setCount((prevCount) => prevCount - 1)}>
+					Decrement
+				</button>
+			</article>
+		</>
+	);
+}
+
+export default App;
